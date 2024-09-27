e7be930561fdc41386263fb2668c479818bfaa1e
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-09-27 14:54:32 +0700

    Create mlflow_tracking_server.py

---
+++ mlflow_tracking_server.py
@@ -0,0 +1,17 @@
+import sys
+import mlflow
+import os
+import subprocess
+
+if __name__ == "__main__":
+    print(f"Python executable: {sys.executable}")
+    print(f"MLflow version: {mlflow.__version__}")
+    
+    # Define SQLite URI for model registry and experiment tracking
+    mlflow_tracking_uri = f"sqlite:///{os.path.abspath('mlflow.db')}"
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
+    
+    print(f"MLflow Tracking URI: {mlflow_tracking_uri}")
+    print("Starting MLflow UI. Access it at http://localhost:5000")
+    
+    subprocess.run(["mlflow", "ui", "--port", "5000", "--backend-store-uri", mlflow_tracking_uri])
