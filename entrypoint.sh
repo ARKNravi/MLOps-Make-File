980171d6ed0783c7d7359f3e4e515d7535f463d8
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-10-01 16:16:19 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ source activate mlops-makefile
 # Run the Makefile
 make all
 
-# Start the MLflow server
-python mlflow_tracking_server.py
+# Start the MLflow server and make it accessible from outside the container
+mlflow ui --backend-store-uri sqlite:///mlflow.db --host 0.0.0.0 --port 5000
