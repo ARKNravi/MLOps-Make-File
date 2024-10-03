de4fffe3152194dcccee75088ade6cb0c622878d
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-10-03 20:40:52 +0700

    Fixing Port Part 2

--- mlflow_tracking_server.py
+++ mlflow_tracking_server.py
@@ -2,23 +2,22 @@ import sys
 import mlflow
 import os
 import subprocess
-
 import time
 
 if __name__ == "__main__":
     start_time = time.time()
     print(f"Python executable: {sys.executable}")
     print(f"MLflow version: {mlflow.__version__}")
-    
+
     mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI", f"sqlite:///{os.path.abspath('mlflow.db')}")
     mlflow.set_tracking_uri(mlflow_tracking_uri)
-    
+
     print(f"MLflow Tracking URI: {mlflow_tracking_uri}")
     print("Starting MLflow UI. Access it at http://localhost:6000")
-    
+
     mlflow_start_time = time.time()
-    # Add '--host 0.0.0.0' to allow external connections when running locally
+    # Start MLflow UI with the specified port (6000)
     subprocess.run(["mlflow", "ui", "--port", "6000", "--host", "0.0.0.0", "--backend-store-uri", mlflow_tracking_uri])
-    
+
     print(f"MLflow UI started in {time.time() - mlflow_start_time:.2f} seconds.")
     print(f"Total time: {time.time() - start_time:.2f} seconds.")
