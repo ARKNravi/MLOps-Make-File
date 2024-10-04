6e3d8a5d6e8de08f21f5ecd2a8e42fc0fef20bad
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-10-04 19:27:04 +0700

    "Updated docker-compose.yml to add comment for port mapping and removed newline at end of file. Modified mlflow_tracking_server.py to remove port selection logic and hardcoded port 7000 for MLflow UI."

--- mlflow_tracking_server.py
+++ mlflow_tracking_server.py
@@ -13,17 +13,10 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri(mlflow_tracking_uri)
 
     print(f"MLflow Tracking URI: {mlflow_tracking_uri}")
-    print("Starting MLflow UI. Access it at http://localhost:7000 or http://localhost:7001")
+    print("Starting MLflow UI. Access it at http://localhost:7000")
 
     mlflow_start_time = time.time()
-    
-    port = 7000
-    if subprocess.run(["lsof", "-i", f":{port}"]).returncode == 0:
-        print(f"Port {port} is in use. Trying the next port...")
-        port = 7001
-    
-    print(f"Using port {port}")
-    subprocess.run(["mlflow", "ui", "--port", str(port), "--host", "0.0.0.0", "--backend-store-uri", mlflow_tracking_uri])
+    subprocess.run(["mlflow", "ui", "--port", "7000", "--host", "0.0.0.0", "--backend-store-uri", mlflow_tracking_uri])
 
     print(f"MLflow UI started in {time.time() - mlflow_start_time:.2f} seconds.")
     print(f"Total time: {time.time() - start_time:.2f} seconds.")
