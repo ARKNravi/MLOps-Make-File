123d1132ff3320fac34f4867d1ba303df2e71f2e
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-10-04 15:10:23 +0700

    Update mlflow_tracking_server.py

--- mlflow_tracking_server.py
+++ mlflow_tracking_server.py
@@ -13,11 +13,10 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri(mlflow_tracking_uri)
 
     print(f"MLflow Tracking URI: {mlflow_tracking_uri}")
-    print("Starting MLflow UI. Access it at http://0.0.0.0:6000")
+    print("Starting MLflow UI. Access it at http://0.0.0.0:5000")  # Change to port 5000 or 8080
 
     mlflow_start_time = time.time()
-    # Update the command to bind to 0.0.0.0
-    subprocess.run(["mlflow", "ui", "--host", "0.0.0.0", "--port", "6000", "--backend-store-uri", mlflow_tracking_uri])
+    subprocess.run(["mlflow", "ui", "--host", "0.0.0.0", "--port", "5000", "--backend-store-uri", mlflow_tracking_uri])
 
     print(f"MLflow UI started in {time.time() - mlflow_start_time:.2f} seconds.")
     print(f"Total time: {time.time() - start_time:.2f} seconds.")
